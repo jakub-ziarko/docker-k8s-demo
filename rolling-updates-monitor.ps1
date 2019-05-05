f77c8410016cb54cbc3d26d5e97ec54988536356
Jakub Ziarko <jakub.ziarko@solteq.com>
2019-05-05 10:31:31 +0200

    rolling update monitor

---
+++ rolling-updates-monitor.ps1
@@ -0,0 +1,8 @@
+﻿
+while ($true) {
+    $Response = Invoke-WebRequest 'http://localhost:5000/api/version/' -Method 'GET'
+
+    Write-Host 'Version: '$Response
+
+    Start-Sleep -s 1
+}
---
+++ src/EggOrdersApi/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EggOrdersApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetVersion()
+        {
+            return Ok("Version1");
+        }
+    }
+}
